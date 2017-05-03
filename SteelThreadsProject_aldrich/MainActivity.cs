2796d04ce5b7458af90fcf0ee8c0b4b429f22a90
nick_aldrich <aldrichnick95@gmail.com>
2017-05-03 18:25:47 -0400

    my commit

---
+++ SteelThreadsProject_aldrich/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Configuration;
+using Newtonsoft.Json;
+using Android.Util;
+using System.Net.Http;
+using System.Text;
+
+namespace SteelThreadsProject_aldrich
+{
+    [Activity(Label = "SteelThreadsProject_aldrich", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        TextView textView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            textView = FindViewById<TextView>(Resource.Id.txtInput);
+            Button button = FindViewById<Button>(Resource.Id.btnPost);
+
+            button.Click += btn_Click;
+        }
+
+        private void btn_Click(object sender, System.EventArgs e)
+        {
+
+            string MyText = textView.Text.ToString();
+            string popUp = "Your message has been posted.";
+            Toast.MakeText(this, popUp, ToastLength.Short).Show();
+            if (MyText == "")
+            {
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var url = string.Format("https://nickproject.azurewebsites.net/api/HttpTriggerCSharp1?code=m83CfuegOW5FAHrxzWeAvHiHp3a4guFdbTdeluzXkvPSuBWrbERL/w==" + "&");
+                    var content = new StringContent("{myText:'" + MyText + "'}", Encoding.UTF8, "application/json");
+                    var result = client.PostAsync(url, content).Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result;
+                    //var content = new stringContent({ });
+                    //var intent = new Intent(Intent.ActionView, url);
+                    //StartActivity(intent);
+                }
+            }
+            else
+            {
+                string error = "You haven't entered any text";
+                Toast.MakeText(this, error, ToastLength.Short).Show();
+            }
+        }
+    }
+}
---
+++ SteelThreadsProject_aldrich/obj/Debug/android/src/md53691d19b73734399476fd16c5a546e97/MainActivity.java
@@ -0,0 +1,47 @@
+package md53691d19b73734399476fd16c5a546e97;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("SteelThreadsProject_aldrich.MainActivity, SteelThreadsProject_aldrich, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SteelThreadsProject_aldrich.MainActivity, SteelThreadsProject_aldrich, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
